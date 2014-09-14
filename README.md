2d194fbf70bb19bdc48e1ec79797ab48f58ee611
Brian Low <github@brianlow.com>
2014-09-14 12:19:45 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 FileRename
 ==========
 
-Rename files from the ST3 command palette
+Rename files from the SublimeText3 command palette. No mouse required.
+
+To run
+------
+
+Ctrl-Shift-P -> File:Rename
+
+
+To install
+----------
+
+Copy files into Packages\User\ (see Sublime -> Preferences -> Browse Packages...)
+
+
+
---
+++ file_rename.py
@@ -0,0 +1,27 @@
+import sublime
+import sublime_plugin
+import os
+
+class FileRenameCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filename = self.view.file_name()
+        branch, leaf = os.path.split(filename)
+
+        panel = self.view.window().show_input_panel("New Name:", leaf, functools.partial(self.on_done, filename, branch), None, None)
+
+        name, ext = os.path.splitext(leaf)
+        panel.sel().clear()
+        panel.sel().add(sublime.Region(0, len(name)))
+
+    def on_done(self, old, branch, leaf):
+            new = os.path.join(branch, leaf)
+
+            try:
+                os.rename(old, new)
+
+                v = self.view.window().find_open_file(old)
+                if v:
+                    v.retarget(new)
+            except Exception as e :
+                sublime.status_message("Unable to rename: " + str(e))
+
